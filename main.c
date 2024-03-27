ff26a333d80fae3d06e2181d9bdfb0b4dcc2245c
Logan MacDougall <loganmac3000@hotmail.com>
2024-03-27 01:16:44 -0300

    Previously typed alphabet can be used again by typing '^' for alphabet

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 #define QUIT_CODE 1
 
-void get_alphabet(char *buffer);
+void get_alphabet(char *buffer, bool allow_use_previous);
 void get_knowledge(char *buffer);
 void print_startup_text();
 void print_help_text();
@@ -22,11 +22,12 @@ int main() {
   print_startup_text();
 
   while (1) {
-    get_alphabet(alphabetBuffer);
+    get_alphabet(alphabetBuffer, solver->alphabet != NULL);
     if (alphabetBuffer[0] == QUIT_CODE)
       break;
 
-    unscrabbler_set_alphabet(solver, alphabetBuffer);
+    if (alphabetBuffer[0] != '^' || solver->alphabet == NULL)
+      unscrabbler_set_alphabet(solver, alphabetBuffer);
 
     get_knowledge(knowledgeBuffer);
     if (knowledgeBuffer[0] == QUIT_CODE)
@@ -54,13 +55,13 @@ int main() {
   return 0;
 }
 
-void get_alphabet(char *buffer) {
+void get_alphabet(char *buffer, bool allow_use_previous) {
   printf("Provide an alphabet: ");
   scanf("%s", buffer);
 
   if (strncmp(buffer, "!help", 5) == 0) {
     print_help_text();
-    return get_alphabet(buffer);
+    return get_alphabet(buffer, allow_use_previous);
   }
 
   if (strncmp(buffer, "!quit", 5) == 0) {
@@ -68,12 +69,15 @@ void get_alphabet(char *buffer) {
     return;
   }
 
+  if (buffer[0] == '^' && allow_use_previous)
+    return;
+
   for (int i = 0; buffer[i] != '\0'; i++) {
     if (buffer[i] == '\n') {
       buffer[i] = '\0';
     } else if (buffer[i] < 'a' || buffer[i] > 'z') {
       printf("Character %c is invalid\n", buffer[i]);
-      return get_alphabet(buffer);
+      return get_alphabet(buffer, allow_use_previous);
     }
   }
 }
@@ -119,6 +123,10 @@ void print_help_text() {
   puts("The word \"hello\" has the alphabet \"ehllo\", the order of");
   puts("the letters doesn't matter, but you need to include duplicates");
   puts("If you want the letter to be used multiple times.\n");
+
+  puts("If you want to use the same alphabet that you used last time instead");
+  puts("of typing it over again, you can type the alphabet as '^' instead of");
+  puts("a valid new alphabet\n");
 
   puts("When prompted about filling in knowledge about the word, you");
   puts("Can put the letters you know, as well as stars for what you don't");
